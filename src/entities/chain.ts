d15527c72abb31f4f3b7718d94862b940018db53
Coffee Crypto Indo <coffeecryptoindo@gmail.com>
2023-03-27 21:48:44 +0800

    add coredao and matic

--- src/entities/chain.ts
+++ src/entities/chain.ts
@@ -1,5 +1,9 @@
 export enum ChainId {
   MAINNET = 1,
+  GÖRLI = 5,
+  MATIC = 137,
+  MUMBAI = 80001,
+  BSCTESTNET = 97,
   BSCMAINNET = 56,
   SOLANA = 101, //fake id
   COREDAO = 1116,
--- src/entities/weth.ts
+++ src/entities/weth.ts
@@ -16,6 +16,28 @@ export const WETH = {
     'WBNB',
     'Wrapped BNB',
   ),
+  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0x48f6D7dAE56623Dde5a0D56B283165cAE1753D70', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.MATIC]: new Token(
+    ChainId.MATIC,
+    '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+    18,
+    'WMATIC',
+    'Wrapped Matic',
+  ),
+  [ChainId.MUMBAI]: new Token(
+    ChainId.MUMBAI,
+    '0x19395624C030A11f58e820C3AeFb1f5960d9742a',
+    18,
+    'WMATIC',
+    'Wrapped Matic',
+  ),
+  [ChainId.BSCTESTNET]: new Token(
+    ChainId.BSCTESTNET,
+    '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+    18,
+    'WBNB',
+    'Wrapped BNB',
+  ),
   [ChainId.COREDAO]: new Token(
     ChainId.COREDAO,
     '0x40375C92d9FAf44d2f9db9Bd9ba41a3317a2404f',
